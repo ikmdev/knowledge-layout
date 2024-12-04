1621ab2fca592f8f3ffebded24285d23d14bc6ae
Andrew Sills <andrew.k.sills@gmail.com>
2024-12-04 13:44:34 -0500

    adding additional jpms exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,19 @@ module dev.ikm.layout {
 
     opens dev.ikm.layout to javafx.fxml;
     exports dev.ikm.layout;
+    exports dev.ikm.layout.action;
+    exports dev.ikm.layout.component;
+    exports dev.ikm.layout.component.version.field;
+    exports dev.ikm.layout.component.version;
+    exports dev.ikm.layout.container;
+    exports dev.ikm.layout.context;
+    exports dev.ikm.layout.coordinate;
+    exports dev.ikm.layout.event;
+    exports dev.ikm.layout.orchestration;
+    exports dev.ikm.layout.orchestration.process;
+    exports dev.ikm.layout.selection;
+    exports dev.ikm.layout.selection.element;
+
     exports dev.ikm.layout.app;
     opens dev.ikm.layout.app to javafx.fxml;
 }
